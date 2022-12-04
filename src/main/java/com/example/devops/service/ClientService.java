02ecb8ddb202a75bc16233820661b03be7102d91
leith <leith.mhf@gmail.com>
2022-12-04 18:58:18 +0100

    client service implementation

--- src/main/java/com/example/devops/service/ClientService.java
+++ src/main/java/com/example/devops/service/ClientService.java
@@ -3,10 +3,11 @@ package com.example.devops.service;
 import com.example.devops.entity.ClientEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ClientService {
     List<ClientEntity> findAllClients();
-    ClientEntity findById(Long id);
+    Optional<ClientEntity> findById(Long id);
     ClientEntity addClient(ClientEntity client);
     ClientEntity updateClient(ClientEntity client);
     void deleteClient(Long id);
--- src/main/java/com/example/devops/service/impl/ClientServiceImpl.java
+++ src/main/java/com/example/devops/service/impl/ClientServiceImpl.java
@@ -1,4 +1,44 @@
 package com.example.devops.service.impl;
 
-public class ClientServiceImpl {
+import com.example.devops.entity.ClientEntity;
+import com.example.devops.repository.ClientRepository;
+import com.example.devops.service.ClientService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClientServiceImpl implements ClientService {
+    private final ClientRepository clientRepository;
+
+    public ClientServiceImpl(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @Override
+    public List<ClientEntity> findAllClients() {
+        return clientRepository.findAll();
+    }
+
+    @Override
+    public Optional<ClientEntity> findById(Long id) {
+        return clientRepository.findById(id);
+    }
+
+    @Override
+    public ClientEntity addClient(ClientEntity client) {
+        return clientRepository.save(client);
+    }
+
+    @Override
+    public ClientEntity updateClient(ClientEntity client) {
+        return clientRepository.save(client);
+    }
+
+    @Override
+    public void deleteClient(Long id) {
+        clientRepository.deleteById(id);
+
+    }
 }
