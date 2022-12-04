02714c80d3108eea03b70bbf2d58fbd689e72720
leith <leith.mhf@gmail.com>
2022-12-04 18:52:20 +0100

    client service

--- src/main/java/com/example/devops/service/ClientService.java
+++ src/main/java/com/example/devops/service/ClientService.java
@@ -1,4 +1,14 @@
 package com.example.devops.service;
 
+import com.example.devops.entity.ClientEntity;
+
+import java.util.List;
+
 public interface ClientService {
+    List<ClientEntity> findAllClients();
+    ClientEntity findById(Long id);
+    ClientEntity addClient(ClientEntity client);
+    ClientEntity updateClient(ClientEntity client);
+    void deleteClient(Long id);
+
 }
