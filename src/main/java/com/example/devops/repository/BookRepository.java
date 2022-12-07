1e984ed6a0644b1db9f2428066e889950cade977
leith <leith.mhf@gmail.com>
2022-12-07 23:21:13 +0100

    repository update

--- src/main/java/com/example/devops/repository/BookRepository.java
+++ src/main/java/com/example/devops/repository/BookRepository.java
@@ -3,5 +3,5 @@ package com.example.devops.repository;
 import com.example.devops.entity.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<BookEntity,Long> {
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
 }
