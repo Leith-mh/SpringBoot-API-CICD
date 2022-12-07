de2aef77eef0467e1d766a76704db551abc6306d
Leith Mahfoudhi <76526645+Leith-mh@users.noreply.github.com>
2022-12-07 21:38:48 +0100

    Update functions.groovy

--- functions.groovy
+++ functions.groovy
@@ -12,7 +12,7 @@ def cloneCode() {
 } 
 def buildJar() {
     echo 'building the application...'
-    //sh 'mvn -f ./pom.xml clean package'
+    sh 'mvn -f ./pom.xml clean package'
     sh 'mvn -version'
 } 
 return this
