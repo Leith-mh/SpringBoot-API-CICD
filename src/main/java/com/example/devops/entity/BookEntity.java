c726a29d617e43bbf1d67632bc39aa1396efbded
leith <leith.mhf@gmail.com>
2022-12-07 23:20:49 +0100

    javax persistence

--- src/main/java/com/example/devops/entity/BookEntity.java
+++ src/main/java/com/example/devops/entity/BookEntity.java
@@ -1,49 +1,30 @@
 package com.example.devops.entity;
 
-import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
-@Table(name="book")
+@Table(name = "book")
 public class BookEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    private String isbn;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "author")
     private String author;
 
-    public BookEntity() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+    @Column(name = "genre")
+    private String genre;
 }
