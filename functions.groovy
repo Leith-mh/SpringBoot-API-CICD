27b5238b5c888782c896aecb39c90997a2d93879
Leith Mahfoudhi <76526645+Leith-mh@users.noreply.github.com>
2022-12-08 22:09:28 +0100

    Update functions.groovy

--- functions.groovy
+++ functions.groovy
@@ -12,26 +12,60 @@ def cloneCode() {
 } 
 
 def unitTest() {
-    echo 'building the application...'
-    sh 'mvn test'
+    echo 'testing the application...'
 } 
 def buildJar() {
     echo 'building the application...'
     sh 'mvn -f ./pom.xml clean package'
     sh 'mvn -version'
 } 
+
 def buildImage(String imageName) {
     echo 'building the docker image...'
-    //getting credentials of github from jenkins
-    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        //building the image with dockerhub repo tag
-        sh " docker build -t $imageName ."
-     
+    sh " docker build -t $imageName ."
+}
+
+def pushImageDocker(String imageName) {
+   echo 'pushing the docker image to dockerhub'
+   withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         //login to dockerhub
         sh "echo $PASS |  docker login -u $USER --password-stdin"
         //pushing the image to dockerhub
         sh " docker push $imageName"
-    
     }
 }
+
+def pushImageNexus(String NEXUS_VERSION,String NEXUS_PROTOCOL,String NEXUS_URL,String NEXUS_REPOSITORY,String NEXUS_CREDENTIAL_ID)
+{
+  pom = readMavenPom file: "pom.xml";
+                    filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
+                    echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
+                    artifactPath = filesByGlob[0].path;
+                    artifactExists = fileExists artifactPath;
+                    if(artifactExists) {
+                        echo "*** File: ${artifactPath}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}";
+                        nexusArtifactUploader(
+                            nexusVersion: $NEXUS_VERSION,
+                            protocol: $NEXUS_PROTOCOL,
+                            nexusUrl: $NEXUS_URL,
+                            groupId: pom.groupId,
+                            version: pom.version,
+                            repository: $NEXUS_REPOSITORY,
+                            credentialsId: $NEXUS_CREDENTIAL_ID,
+                            artifacts: [
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: artifactPath,
+                                type: pom.packaging],
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: "pom.xml",
+                                type: "pom"]
+                            ]
+                        );
+                    } else {
+                        error "*** File: ${artifactPath}, could not be found";
+                    }
+}
+   
 return this
